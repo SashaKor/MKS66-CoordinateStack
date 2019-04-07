86482783234332324b1c37f60f9b17b88104e38a
SashaKor <akoroza@stuy.edu>
2019-04-07 19:29:28 -0400

    comment

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ t = new_matrix()
 ident(t)
 csystems = [ t ]
 
-#parse_file( 'script', edges, polygons, csystems, screen, color )
-parse_file( 'script2', edges, polygons, csystems, screen, color )
+parse_file( 'script', edges, polygons, csystems, screen, color )
+#parse_file( 'script2', edges, polygons, csystems, screen, color )
