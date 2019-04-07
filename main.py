2b4fdd26ff9d01c092e5e97217b7848ef182578e
SashaKor <akoroza@stuy.edu>
2019-04-07 19:27:34 -0400

    mitosis script

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from matrix import *
 import math
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 0 ]
 edges = []
 polygons = []
 t = new_matrix()
 ident(t)
 csystems = [ t ]
 
-parse_file( 'script', edges, polygons, csystems, screen, color )
+#parse_file( 'script', edges, polygons, csystems, screen, color )
+parse_file( 'script2', edges, polygons, csystems, screen, color )
